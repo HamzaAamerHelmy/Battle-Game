5354fb86246e56ca0fe4be7fb20d44b91b739a85
Hamza Aamer <Hamza.Aamer@outlook.com>
2026-08-19 15:07:23 +0300

    initial commit: add main game logic and menu structure

---
+++ mian.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+
+
+struct stPlayer
+{
+    int playerHP     = 100;
+    int playerChoose = 0;
+};
+
+struct stComputer
+{
+    int computerHP     = 100;
+    int computerChoose = 0;
+};
+
+int randomNumber(int from, int to)
+{
+    to += 1;
+    int randNum = rand() % (to - from) + from;
+}
+
+void printMainMenuScreen()
+{
+    cout << "========================================" << endl << endl;
+    cout << "              BATTLE GAME               " << endl << endl;
+    cout << "========================================" << endl << endl << endl;
+    cout << "       1. Start Game" << endl;
+    cout << "       2. How To Play" << endl;
+    cout << "       3. Exit" << endl << endl << endl;
+    cout << "========================================" << endl << endl;
+}
+
+short readMainMenuChoose()
+{
+    short userInput = 0;
+
+    do
+    {
+        cout << "Choose: ";
+        cin >> userInput;
+
+        if (userInput > 3 || userInput < 1)
+        {
+            cout << "Wrong Input!" << endl;
+            cout << "Please Enter A valid Number ..." << endl << endl;
+        }
+        
+    } while (userInput > 3 || userInput < 1);
+    
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    stPlayer player;
+
+    stComputer computer;
+
+    printMainMenuScreen();
+    short choose = readMainMenuChoose();
+
+    return 0;
+}
